32e13341b9d460a83c517303c88071d8314ccd39
viggo <github@cviggo.com>
2014-06-28 19:58:18 +0200

    * another attempt to fix #22

--- src/main/java/viggo/omniscient/DatabaseEngine.java
+++ src/main/java/viggo/omniscient/DatabaseEngine.java
@@ -305,7 +305,7 @@ public class DatabaseEngine implements Runnable {
 
                     case Delete:
                         sql = String.format(
-                                "DELETE FROM BlockInfo WHERE (`world`, `x`, `y`, `z`) IN (%s, %d, %d, %d)",
+                                "DELETE FROM BlockInfo WHERE (`world`, `x`, `y`, `z`) IN ('%s', %d, %d, %d)",
                                 blockInfo.world,
                                 blockInfo.x,
                                 blockInfo.y,
